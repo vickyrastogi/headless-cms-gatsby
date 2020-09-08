39e0b32b296f7200de6853ecaf0225177be0b905
Vikas.Rastogi <vikas.rastogi@pepsico.com>
2020-09-08 11:07:10 +0530

    removed more

--- src/pages/index.js
+++ src/pages/index.js
@@ -130,42 +130,6 @@ class IndexPage extends React.Component {
             </div>
           </Container>
 
-          <div
-            css={{
-              background: constants.darkYellow
-            }}
-          >
-            <Container
-              css={{
-                paddingLeft: rhythm(4)
-              }}
-            >
-              <div css={{ maxWidth: rhythm(15) }}>
-                <h2>In this {`month's`} edition</h2>
-                <p>
-                  Quisque vitae pulvinar arcu. Aliquam ac pellentesque erat, at
-                  finibus massa.
-                </p>
-                <p>
-                  {` `}
-                  Suspendisse eget • leo sed • felis commodo • semper id • sed
-                  erat
-                </p>
-                <button
-                  css={{
-                    background: constants.darkGray,
-                    color: gray(75, 0, true),
-                    padding: `${rhythm(1 / 3)} ${rhythm(2 / 3)}`,
-                    lineHeight: 1.3,
-                    cursor: `pointer`
-                  }}
-                >
-                  More Umami
-                </button>
-              </div>
-            </Container>
-          </div>
-
           <Container>
             <h1 css={{ textAlign: `center` }}>Recipes</h1>
             <h2 css={{ textAlign: `center` }}>
